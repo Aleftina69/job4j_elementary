32dd9ebd7df6b30f345660012a5e6ce9b8f2b5e9
Brux <okladno94@gmail.com>
2024-04-16 20:31:01 +0500

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Алефтина Тишакова");
+        System.out.println("30.05.1994");
+    }
+}
