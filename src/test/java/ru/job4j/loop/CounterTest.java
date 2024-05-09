7f68ff16a0fac37623fbbcff90d435f986d9b2f5
Brux <okladno94@gmail.com>
2024-05-09 23:08:59 +0500

    5.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,13 +1,11 @@
 package ru.job4j.loop;
 
 import org.junit.jupiter.api.Test;
-import ru.job4j.condition.ThreeMax;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class CounterTest {
     @Test
-    void whenStart7Finish23Sum255() {
+    void whenSumNumbersFrom7To23Then255() {
         int start = 7;
         int finish = 23;
         int result = Counter.sum(start, finish);
@@ -16,11 +14,29 @@ class CounterTest {
     }
 
     @Test
-    void whenStart18Finish5Sum0() {
+    void whenSumNumbersStart18Finish5Then0() {
         int start = 18;
         int finish = 5;
         int result = Counter.sum(start, finish);
         int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom1To10Then30() {
+        int start = 1;
+        int finish = 10;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenSumEvenNumbersFrom3To8Then18() {
+        int start = 3;
+        int finish = 8;
+        int result = Counter.sumByEven(start, finish);
+        int expected = 18;
         assertThat(result).isEqualTo(expected);
     }
 }
