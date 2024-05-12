f4e766e46a3eac7c602fa92a3a2d298d66195de2
Brux <okladno94@gmail.com>
2024-05-12 18:04:55 +0500

    6.4.0. Слово начинается с ... [#41585 #495606]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -2,12 +2,11 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
-        boolean result = true;
         for (int index = 0; index < prefix.length; index++) {
             if (prefix[index] != word[index]) {
                 return false;
             }
         }
-        return result;
+        return true;
     }
 }
